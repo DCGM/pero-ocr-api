c716e587d1418b8f8cef3090972995d9c89e2366
Michal Hradis <michal.hradis@gmail.com>
2022-06-13 11:57:54 +0200

    Can send anonymous mail.

--- app/mail/mail.py
+++ app/mail/mail.py
@@ -2,7 +2,10 @@ from drymail import SMTPMailer, Message
 
 
 def send_mail(subject, body, sender, password, recipients, host):
-    client = SMTPMailer(host=host, user=sender[1], password=password, tls=True)
+    if password:
+        client = SMTPMailer(host=host, user=sender[1], password=password, tls=True)
+    else:
+        client = SMTPMailer(host=host)
     message = Message(subject=subject, sender=sender,
                       receivers=recipients, html=body)
     client.send(message)
