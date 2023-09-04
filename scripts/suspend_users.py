d5bea0c49986c8cbd6e72055434be5d56aaf5486
Pavel Raur <pavel.raur@gmail.com>
2023-09-04 19:23:45 +0200

    Removed obsolete SQLAlchemy param

--- scripts/suspend_users.py
+++ scripts/suspend_users.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
     args = get_args()
 
     engine = create_engine(f'{args.database}',
-                           convert_unicode=True,
                            connect_args={})
     db_session = scoped_session(sessionmaker(autocommit=False,
                                              autoflush=False,
